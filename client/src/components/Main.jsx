11489a655f85db0343a8742ef90c76ec458f5865
Sumeet Gupta <sumeetgupta3690@gmail.com>
2024-02-18 03:22:35 +0530

    Atharva_HomeTopBar_14

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const Main = () => {
-  return (
-    <div id='mainHome'>
-   
-    </div>
-  )
+    return (
+        <div id='mainHome'>
+            <div id="topBar">
+
+                <div id="selBox">
+                    <div id='selCourse'>Courses</div>
+                    <div id='selGames'>Games</div>
+                </div>
+                <div id="selIcon">
+                <i class="fa-solid fa-trophy"></i>
+                <i class="fa-solid fa-user"></i>
+                </div>
+
+
+            </div>
+        </div>
+    )
 }
 
 export default Main
