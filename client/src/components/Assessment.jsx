391a7d218c650e5435db50e88040377fee41bb26
Sumeet Gupta <sumeetgupta3690@gmail.com>
2024-02-19 09:42:24 +0530

    Update

--- client/src/components/Assessment.jsx
+++ client/src/components/Assessment.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 
-const Roadmap = () => {
+const Assessment = () => {
   return (
     <div>
       
@@ -9,4 +9,4 @@ const Roadmap = () => {
   )
 }
 
-export default Roadmap
+export default Assessment
